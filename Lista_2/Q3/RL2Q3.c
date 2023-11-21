9ff041a23769fee837adf26bbf9402a9aee84d61
DeivisonCs <deivisonlx@DevisCs>
2023-11-21 11:06:37 -0300

    Q3 Finalizada

--- Lista_2/Q3/RL2Q3.c
+++ Lista_2/Q3/RL2Q3.c
@@ -12,7 +12,7 @@ typedef struct no {
     struct no *father;
     struct no *right;
     struct no *left;
-    int key;
+    int height, key;
 }Tree_Node;
 
 typedef struct head {
@@ -21,10 +21,16 @@ typedef struct head {
 
 
 void init(Tree_Root *root);
+int get_maior(Tree_Node *node);
 void tree_free(Tree_Node *node);
+void show_tree(Tree_Node *node);
 Tree_Node* create_node(int value);
-Tree_Node* get_next(Tree_Node *node);
+void transfer_2_file(Tree_Root *Root, FILE* arq_in);
+Tree_Node* get_sucesor(Tree_Node *node);
+Tree_Node* search_node(Tree_Node *node, int value);
+void delete_element(Tree_Root *Root, int value);
 void insert_node(Tree_Root *root, Tree_Node *new_node);
+void transefer_node(Tree_Node *node, FILE* arq_out, int maior_Atual);
 
 
 int  main() {
@@ -50,18 +56,23 @@ int  main() {
         init(_Root);
         while(token != NULL)
         {
-
             if(strcmp(token, "a") == 0){
+
                 token = strtok(NULL, separador);
-                printf("%s ", token);
+                insert_node(_Root, create_node(atoi(token)));
             }
             else if(strcmp(token, "r") == 0){
+                
                 token = strtok(NULL, separador);
-                printf("%s ", token);
+                delete_element(_Root, atoi(token));
             }
+            // show_tree(_Root->root);
+            // printf("\n");
 
             token = strtok(NULL, separador);
         }
+        transfer_2_file(_Root, arq_in);
+        tree_free(_Root->root);
 
     }while(!feof(arq_in));
 
@@ -89,35 +100,26 @@ Tree_Node* create_node(int value){
 void insert_node(Tree_Root *root, Tree_Node *new_node){
     Tree_Node *x = NULL;
     Tree_Node *y = root->root;
+    int alt = 0;
 
     while(y != NULL)
     {
         x = y;
+        alt++;
 
-        if(new_node->key < y->key) y = y->left;
-        else if(new_node->key > y->key) y = y->right;
-
-        else {  // Não permite valores iguais
-            free(new_node);
-            return;
-        }
+        if(new_node->key < x->key) y = y->left;
+        else y = y->right;
     }
 
     new_node->father = x;
+    new_node->height = alt;
 
     if(x == NULL) root->root = new_node;
-    if(new_node->key < x->key) x->left = new_node;
-    else x->left = new_node;
+    else if(new_node->key < x->key) x->left = new_node;
+    else x->right = new_node;
 }
 
-// void remove_node(Tree_Root *Root, int value){
-
-//     Tree_Node *node = Root->root;
-
-
-// }
-
-Tree_Node* get_next(Tree_Node *node){
+Tree_Node* get_sucesor(Tree_Node *node){
 
     Tree_Node *min = node;
     
@@ -126,8 +128,110 @@ Tree_Node* get_next(Tree_Node *node){
 
         while(min->left != NULL)
             min = min->left;
+
+        return min;
     }
 
+    Tree_Node *aux = node->father;
+    while(aux != NULL && aux == node->right){
+        node = aux;
+        aux = aux->father;
+    }
+
+    return aux;
+}
+
+void delete_element(Tree_Root *Root, int value){
+
+    Tree_Node *node = search_node(Root->root, value);
+    Tree_Node *to_Delete;
+    Tree_Node *x;
+
+    if(node == NULL){
+        insert_node(Root, create_node(value));
+        return;
+    }
+
+    if(node->left == NULL || node->right == NULL) to_Delete = node;
+    else to_Delete = get_sucesor(node);
+
+    if(to_Delete->left != NULL) x = to_Delete->left;
+    else x = to_Delete->right;
+
+    if(x != NULL) x->father = to_Delete->father;
+
+    if(to_Delete->father == NULL) Root->root = x;
+    else{
+        if(to_Delete == to_Delete->father->left) to_Delete->father->left = x;
+        else to_Delete->father->right = x;
+    }
+
+    if(to_Delete != node) node->key = to_Delete->key;
+
+    free(to_Delete); 
+}
+
+Tree_Node* search_node(Tree_Node *node, int value){
+    
+    if(node != NULL){
+
+        if(value < node->key) return search_node(node->left, value);
+        if(value > node->key) return search_node(node->right, value);
+        
+        return node;
+    }
+
+    return NULL;
+}
+
+void transfer_2_file(Tree_Root *Root, FILE* arq_in){
+    FILE* arq_out; arq_out = fopen("L2Q3.out", "a");
+
+    if(Root->root != NULL) {
+        int maior_Atual = get_maior(Root->root);
+
+        transefer_node(Root->root, arq_out, maior_Atual);
+
+        if(!feof(arq_in))
+            fprintf(arq_out, "\n");
+    }
+
+    fclose(arq_out);
+}
+
+void transefer_node(Tree_Node *node, FILE* arq_out, int maior_Atual){
+
+    if(node != NULL){
+
+        transefer_node(node->left, arq_out, maior_Atual);
+
+        if(node->key == maior_Atual && node->right == NULL)
+            fprintf(arq_out, "%d (%d)", node->key, node->height);
+        
+        else
+            fprintf(arq_out, "%d (%d) ", node->key, node->height);
+        
+
+        transefer_node(node->right, arq_out, maior_Atual);
+    }
+}
+
+int get_maior(Tree_Node *node){
+
+    while(node->right != NULL)
+        node = node->right;
+
+    return node->key;
+}
+
+void show_tree(Tree_Node *node){
+
+    if(node != NULL){
+
+        show_tree(node->left);
+        printf("%d (%d) ", node->key, node->height);
+        show_tree(node->right);
+    }
 }
 
 void tree_free(Tree_Node *node){
